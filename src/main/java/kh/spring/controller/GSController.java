a88be4082eb6f4fd13b65aaaa8b6ad6bad4d408d
kgs <rlarmstn71@naver.com>
2021-01-07 11:50:17 +0900

    GS

--- src/main/java/kh/spring/controller/GSController.java
+++ src/main/java/kh/spring/controller/GSController.java
@@ -1,9 +1,14 @@
 package kh.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GSController {
 
+	@RequestMapping
+	public void wow() {
+		System.out.println("신통방통 ㅋㅋㅋ");
+	}
 	
 }
