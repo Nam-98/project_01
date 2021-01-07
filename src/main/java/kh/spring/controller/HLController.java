90a3015459b226cfd34f40876a11f0b4fa064f1c
tester <test@test.com>
2021-01-07 11:36:50 +0900

    HLController ABC Content Added

--- src/main/java/kh/spring/controller/HLController.java
+++ src/main/java/kh/spring/controller/HLController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class HLController {
-
 	public void surprise() {
 		System.out.println("신기합니다");
+		System.out.println("ABC");
 	}
 }
